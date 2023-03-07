94cb58849183aaa0fdb1393c59e27ff1f48c1902
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-03-07 08:49:37 -0300

    feat
    req dois feito, model User implementada

--- src/models/User.js
+++ src/models/User.js
@@ -6,6 +6,7 @@
 
 module.exports = (sequelize, DataTypes) => {
   const UserModel = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     display_name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
